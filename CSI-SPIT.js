ab899eeca33f021f144d785fc55be40cf59b05de
AnanyaNavelkar <ananya.navelkar@gmail.com>
2016-02-22 20:07:41 +0530

    Update CSI-SPIT.js

--- CSI-SPIT.js
+++ CSI-SPIT.js
@@ -6,7 +6,7 @@ $('document').ready(function(){
 	duration:2500,			angle: 0,
 				animateTo:135,
 callback:function(){
-window.location.href="file:///C:/Users/Ananya/Project/home.html";
+window.location.href="home.html";
 }			})
 		}
 	}});
